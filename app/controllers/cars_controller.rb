fe164e176a16114ee73d4489a3fdc1c19b84a95d
github username <github email address>
2018-02-19 11:37:38 -0700

    created car controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,31 @@
 class CarsController < ApplicationController
+  def new
+    @car = Car.new
+  end
+
+  def index
+    @cars = Car.all
+  end
+
+  def create
+    if @car.valid?
+      @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require[:car].permit(:make,:model,:year,:color)
+  end 
+
 end
---
+++ db/migrate/20180217001326_add_hometown_to_user.rb
@@ -0,0 +1,5 @@
+class AddHometownToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :hometown, :string
+  end
+end
